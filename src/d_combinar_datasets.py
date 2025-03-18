1534ec49eabfd464a8b67d41b2bd9b9163a25da9
Alonso Veliz <alonsovelizgarcia12@gmail.com>
2025-03-18 16:52:18 +0100

    Droppeo de las columnas con valores a 0

--- src/d_combinar_datasets.py
+++ src/d_combinar_datasets.py
@@ -8,10 +8,12 @@ def main():
     df_combinado = pd.DataFrame()
 
     for csv_file in files:
-        df_combinado = pd.concat([df_combinado, pd.read_csv(csv_file)], ignore_index=True, )
+        df_combinado = pd.concat([df_combinado, pd.read_csv(csv_file)], ignore_index=True)
 
+    # Eliminar columnas con valores a 0
+    df_combinado = df_combinado.drop(["Bwd PSH Flags", "Bwd URG Flags", "Fwd Bytes/Bulk Avg","Fwd Packet/Bulk Avg", "Fwd Bulk Rate Avg"], axis = 1)
     df_combinado.to_csv(base_folder + "\\combinado.csv", index=False)
 
-
+    # A partir de este dataset iteraremos para cada layer del clasificador creando conjuntos de datos especificos para cada uno de ellos
 if __name__ == "__main__":
     main()
