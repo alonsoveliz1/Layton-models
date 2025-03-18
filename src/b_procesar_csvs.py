e17afee990c87858511adb08713bc1cb107d46f4
Alonso Veliz <alonsovelizgarcia12@gmail.com>
2025-03-18 16:51:11 +0100

    Mappeo de cada clase de ataque en su categoria y eliminacion de valores negativos del dataset

--- src/b_procesar_csvs.py
+++ src/b_procesar_csvs.py
@@ -2,7 +2,7 @@ import datetime
 import os
 import pandas as pd
 
-def parsar_fecha_flujo(timestamp_str) -> datetime:
+def parsear_fecha_flujo(timestamp_str) -> datetime:
 
     # Primero, eliminamos los espacios adicionales
     timestamp_str = timestamp_str.strip()
@@ -27,7 +27,7 @@ def parsar_fecha_flujo(timestamp_str) -> datetime:
         print("Intentando parseo automático")
         return pd.to_datetime(timestamp_str)
     except Exception as e:
-        # print(f"Todos los formatos fallaron: {e}")
+        print(f"Todos los formatos fallaron: {e}")
         return timestamp_str
 
 
@@ -35,7 +35,7 @@ def parsar_fecha_flujo(timestamp_str) -> datetime:
 def procesar_ficheros_csv(archivo_csv: str, target_path: str) -> None:
     """Procesamiento del dataset incluye el mapeo de puertos en los servicios utilizados, eliminacion de columnas innecesarias
         reconversion de tipos de los atributos para que sean consistentes,,,"""
-    # Mapeo de puertos a servicios, 24 servicios entre los mas reconocidos (luego habra que tener en cuenta Otros y los Ephemeral)
+    # Mapeo de puertos a servicios (se irá iterando), 24 servicios al principio entre los mas reconocidos (luego habra que tener en cuenta Otros y los Ephemeral) Ahora menos
     port_service_map = {
         22: "SSH",
         80: "HTTP",
@@ -46,7 +46,44 @@ def procesar_ficheros_csv(archivo_csv: str, target_path: str) -> None:
         443: "HTTPS",
     }
 
-    # Atributos que van a querir ser procesados
+    # Mapeo de cada clase de ataque
+    dos_map = {'DoS TCP Flood', 'DoS SYN Flood', 'SYN Flood', 'DoS DNS Flood'}
+    ddos_map = {'DDoS RSTFIN Flood', 'DDoS TCP SYN Flood', 'ACK Flood', 'DDoS ACK Fragmentation', 'DDoS PSHACK Flood', 'DDoS HTTP Flood'}
+    brute_force_map = {'MQTT Brute Force', 'Sparta SSH Brute Force','Password Attack', 'Dictionary Brute Force','Telnet Brute Force'}
+    scanning_map = {'Recon Port Scan','Recon OS Scan','Recon Vulnerability Scan','Recon Ping Sweep','Scan Aggressive','Port Scanning','Scan Port OS','Scan Host Port','Vulnerability Scanner','Recon Host Discovery','OS Fingerprinting'}
+    application_map = {'XSS','SQL Injection','Uploading Attack'}
+    mitm_map = {'MITM','MITM ARP Spoofing'}
+    mirai_map = {'Mirai ACK Flood','Mirai HTTP Flood','Mirai Host Brute Force'}
+    mqtt_map = {'MQTT DDoS Publish Flood','MQTT DoS Connect Flood','MQTT Malformed','MQTT DoS Publish Flood'}
+    other_map = {'Backdoor','Ransomware'}
+    benign_map = {'Benign Traffic'}
+
+    def classify_attack(attack_name):
+        if attack_name in dos_map:
+            return 'DoS'
+        elif attack_name in ddos_map:
+            return 'DDoS'
+        elif attack_name in brute_force_map:
+            return 'Brute Force'
+        elif attack_name in scanning_map:
+            return 'Scanning'
+        elif attack_name in application_map:
+            return 'Application'
+        elif attack_name in mitm_map:
+            return 'Mitm'
+        elif attack_name in mirai_map:
+            return 'Mirai'
+        elif attack_name in mqtt_map:
+            return 'MQTT'
+        elif attack_name in other_map:
+            return 'Other'
+        elif attack_name in benign_map:
+            return 'Benign'
+        else:
+            return 'Unknown'
+
+
+
     columns_to_drop = ['Flow ID', 'Src IP', 'Src Port', 'Dst IP', 'Dst Port', 'Protocol', 'Device']
 
     float_columns = ['Idle Std', 'Idle Max', 'Total Length of Fwd Packet', 'Total Length of Bwd Packet',
@@ -75,16 +112,25 @@ def procesar_ficheros_csv(archivo_csv: str, target_path: str) -> None:
 
     # Procesar la columna timestamp si existe (spoiler: existe siempre)
     if 'Timestamp' in df.columns:
-        df['Timestamp'] = df['Timestamp'].apply(parsar_fecha_flujo)
+        df['Timestamp'] = df['Timestamp'].apply(parsear_fecha_flujo)
 
     # Añadir columna de Service
     df['Service'] = df['Dst Port'].apply(map_service)
 
+    # Añadir columna de Categoria de ataque
+    df['Attack Category'] = df['Attack Name'].apply(classify_attack)
+
     # Eliminar columnas especificadas (no aportan al modelo o contienen informacion que se ha explicado en nuevas columnas)
     df = df.drop(columns=[col for col in columns_to_drop if col in df.columns])
 
+    # Eliminar las columnas negativas tambien
+    df_numeric = df.select_dtypes(include='number')
+
+    mask_negative = (df_numeric < 0).any(axis=1)
+
+    df_sin_negativos = df[~mask_negative]
     # Guardar el resultado
-    df.to_csv(target_path, index=False)
+    df_sin_negativos.to_csv(target_path, index=False)
 
 
 
