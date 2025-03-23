7696f2e313b6aad9d06074b818f6d303728ebfe1
Alonso Veliz <alonsovelizgarcia12@gmail.com>
2025-03-23 09:07:59 +0100

    Balancing benign flow Services for the final dataset

--- src/f_gen_balanced_dataset.py
+++ src/f_gen_balanced_dataset.py
@@ -35,15 +35,26 @@ def generar_dataset_balanceado(df) -> None:
 
     balanced_malicious = pd.concat(balanced_categories, ignore_index=True)
 
-    df_benign_bal = resample(
-        df_benign,
-        replace = False,
-        n_samples = n_instances * n_categories,
-        random_state = 42
-    )
+    # We also have to create a balanced subset of the benign traffic, since most of it is from the MQTT service so bias ain't introduced in the model
+    balanced_services = []
+    n_services = df_benign['Service'].nunique()
+    n_instances_benign = n_instances * n_categories
 
-    balanced_total = pd.concat([df_benign_bal,balanced_malicious], ignore_index=True)
+    for service_type, df_serv in df_benign.groupby('Service'):
+        df_benign_bal = resample(
+            df_serv,
+            replace = True,
+            n_samples = int(n_instances_benign/n_services),
+            random_state = 42
+        )
+        balanced_services.append(df_benign_bal)
+
+    balanced_benign = pd.concat(balanced_services, ignore_index=True)
+
+    balanced_total = pd.concat([balanced_benign,balanced_malicious], ignore_index=True)
     balanced_total.to_csv("C:\\Users\\avelg\\PycharmProjects\\NIDS\\data\\processed\\CIC-BCCC-NRC-TabularIoT-2024-MOD\\combinado_balanceado.csv", index = False)
+
+    print(balanced_benign['Service'].value_counts())
 
 def main():
     df = pd.read_csv("../data/processed/CIC-BCCC-NRC-TabularIoT-2024-MOD/combinado.csv", low_memory=False)
