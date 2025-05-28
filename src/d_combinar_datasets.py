5eaeb49f6c85648c239e9d82fc12994fe9fa6470
Alonso Veliz Garcia <alonsovelizgarcia12@gmail.com>
2025-05-29 00:35:32 +0200

    Added pathlib raw & processed dataset

--- src/d_combinar_datasets.py
+++ src/d_combinar_datasets.py
@@ -1,10 +1,13 @@
 import pandas as pd
 from a_verificar_consistencia import find_csv_files
-
+from pathlib import Path
 
 def main():
-    base_folder= "C:\\Users\\avelg\\PycharmProjects\\NIDS\\data\\processed\\CIC-BCCC-NRC-TabularIoT-2024-MOD"
-    files = find_csv_files(base_folder)
+    project_folder = Path(__file__).resolve().parent.parent
+    raw_folder = project_folder / "data" / "raw" / "CIC-BCCC-NRC-TabularIoT-2024"
+    processed_folder = project_folder / "data" / "processed" / "CIC-BCCC-NRC-TabularIoT-2024-MOD"
+
+    files = find_csv_files(processed_folder)
     df_combinado = pd.DataFrame()
 
     for csv_file in files:
@@ -12,7 +15,8 @@ def main():
 
     # Eliminar columnas con valores a 0
     df_combinado = df_combinado.drop(["Bwd PSH Flags", "Bwd URG Flags", "Fwd Bytes/Bulk Avg","Fwd Packet/Bulk Avg", "Fwd Bulk Rate Avg"], axis = 1)
-    df_combinado.to_csv(base_folder + "\\combinado.csv", index=False)
+    df_combinado = df_combinado.drop_duplicates()
+    df_combinado.to_csv(processed_folder / "combinado.csv", index=False)
 
     # A partir de este dataset iteraremos para cada layer del clasificador creando conjuntos de datos especificos para cada uno de ellos
 if __name__ == "__main__":
