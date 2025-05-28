bdaba359364484399f170512a5aa8510ff4c0c09
Alonso Veliz Garcia <alonsovelizgarcia12@gmail.com>
2025-05-29 00:35:57 +0200

    Added shuffle of instances, pathlib

--- src/f_gen_balanced_dataset.py
+++ src/f_gen_balanced_dataset.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.utils import resample
+from pathlib import Path
 
 def generar_dataset_balanceado(df) -> None:
     df_malicious = df[df["Label"] == 1]
@@ -11,16 +12,20 @@ def generar_dataset_balanceado(df) -> None:
     muestras_por_categoria = int(n_instances / n_categories)
     balanced_categories = []
 
+    # Balance by attack type
     for attack_name, df_cat in df_malicious.groupby('Attack Category'):
+        print(f"Processing {attack_name}")
         subattacks = df_cat['Attack Name'].unique()
         n_subattacks = len(subattacks)
 
         muestras_por_subataque = int(muestras_por_categoria/n_subattacks)
         balanced_subattacks = []
 
+        print(f"Muestras por subataque {muestras_por_subataque}")
         for subattack_name, df_sub in df_cat.groupby('Attack Name'):
-
+            print(f"Processing {subattack_name}")
             replace_needed = (len(df_sub) < muestras_por_subataque)
+            print(f"Replace needed {replace_needed}")
             df_sub_bal = resample(
                 df_sub,
                 replace = replace_needed,
@@ -53,13 +58,20 @@ def generar_dataset_balanceado(df) -> None:
     balanced_benign = pd.concat(balanced_services, ignore_index=True)
 
     balanced_total = pd.concat([balanced_benign,balanced_malicious], ignore_index=True)
-    balanced_total.to_csv("C:\\Users\\avelg\\PycharmProjects\\NIDS\\data\\processed\\CIC-BCCC-NRC-TabularIoT-2024-MOD\\combinado_balanceado.csv", index = False)
+    base_path = Path(__file__).resolve().parent.parent
+    final_path = base_path / "data" / "processed" / "CIC-BCCC-NRC-TabularIoT-2024-MOD" /  "combinado_balanceado.csv"
+
+    # Shuffle
+    balanced_total = balanced_total.sample(frac=1, random_state=42).reset_index(drop=True)
+    balanced_total.to_csv(final_path, index = False)
 
     print(balanced_benign['Service'].value_counts())
     print(balanced_total['Service'].value_counts())
 
 def main():
-    df = pd.read_csv("../data/processed/CIC-BCCC-NRC-TabularIoT-2024-MOD/combinado.csv", low_memory=False)
+    base_path = Path(__file__).resolve().parent.parent
+    combinado_path = base_path / "data" / "processed" / "CIC-BCCC-NRC-TabularIoT-2024-MOD" / "combinado.csv"
+    df = pd.read_csv(combinado_path , low_memory=False)
     generar_dataset_balanceado(df)
 
 main()
