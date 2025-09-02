d2757a4a6fcaf4c7657f6f483adf30c7cba3c7a1
Alonso Veliz Garcia <alonsovelizgarcia12@gmail.com>
2025-09-02 11:55:45 +0200

    Final commit, minor changes to comments

--- src/d_combinar_datasets.py
+++ src/d_combinar_datasets.py
@@ -7,16 +7,16 @@ def main():
     raw_folder = project_folder / "data" / "raw" / "CIC-BCCC-NRC-TabularIoT-2024"
     processed_folder = project_folder / "data" / "processed" / "CIC-BCCC-NRC-TabularIoT-2024-MOD"
 
-    files = find_csv_files(processed_folder)
+    files = find_csv_files(raw_folder)
     df_combinado = pd.DataFrame()
 
     for csv_file in files:
         df_combinado = pd.concat([df_combinado, pd.read_csv(csv_file)], ignore_index=True)
 
     # Eliminar columnas con valores a 0
-    df_combinado = df_combinado.drop(["Bwd PSH Flags", "Bwd URG Flags", "Fwd Bytes/Bulk Avg","Fwd Packet/Bulk Avg", "Fwd Bulk Rate Avg"], axis = 1)
-    df_combinado = df_combinado.drop_duplicates()
-    df_combinado.to_csv(processed_folder / "combinado.csv", index=False)
+    #df_combinado = df_combinado.drop(["Bwd PSH Flags", "Bwd URG Flags", "Fwd Bytes/Bulk Avg","Fwd Packet/Bulk Avg", "Fwd Bulk Rate Avg"], axis = 1)
+    #df_combinado = df_combinado.drop_duplicates()
+    df_combinado.to_csv(raw_folder / "combinado.csv", index=False)
 
     # A partir de este dataset iteraremos para cada layer del clasificador creando conjuntos de datos especificos para cada uno de ellos
 if __name__ == "__main__":
